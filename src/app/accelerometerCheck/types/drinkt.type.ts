6291fbad9aa384b9760ff8526baf99b441864fd5
mark.heimer <mark.heimer@t2-informatik.de>
2019-05-13 13:53:20 +0200

    simple changes

--- src/app/accelerometerCheck/types/drinkt.type.ts
+++ src/app/accelerometerCheck/types/drinkt.type.ts
@@ -1,6 +1,5 @@
-export class Drink{
-
-    constructor(public name: string, public percentage: number, public mililiter: number){}
+export class Drink {
+    constructor(public name: string, public percentage: number, public mililiter: number) {}
     toString() {
         return this.name + ' | ' + this.percentage + '%' + ' | ' + this.mililiter + ' ml';
     }
